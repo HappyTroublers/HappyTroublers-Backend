2f3860cb453eb32130512e4eee6a875143d8d9a9
Bruna Sonda <bsonda@gmail.com>
2025-07-10 14:15:41 +0200

    first commit

---
+++ src/main/java/happyTroublers/HappyTroublersApplication.java
@@ -0,0 +1,13 @@
+package happyTroublers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTroublersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyTroublersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/happyTroublers/HappyTroublersApplicationTests.java
@@ -0,0 +1,13 @@
+package happyTroublers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyTroublersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
