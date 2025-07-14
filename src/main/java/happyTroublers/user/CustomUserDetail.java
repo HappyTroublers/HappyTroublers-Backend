f19c6105df171bff704f824b71e5c95e7937072b
J-uds <206530139+J-uds@users.noreply.github.com>
2025-07-14 15:36:24 +0200

    feat(user): add methods to customuser

--- src/main/java/happyTroublers/user/CustomUserDetail.java
+++ src/main/java/happyTroublers/user/CustomUserDetail.java
@@ -1,4 +1,31 @@
 package happyTroublers.user;
 
-public class CustomUserDetail {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetail implements UserDetails {
+    private final CustomUser customUser;
+
+    public CustomUserDetail(CustomUser customUser) {
+        this.customUser = customUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority());
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
