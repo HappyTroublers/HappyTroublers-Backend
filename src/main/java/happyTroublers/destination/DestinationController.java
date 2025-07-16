285f73477d6991de45860cf4bfd97ca3dda2808d
Bruna Sonda <bsonda@gmail.com>
2025-07-16 16:38:44 +0200

    feat(DestinationController): add CRUD methods

--- src/main/java/happyTroublers/destination/DestinationController.java
+++ src/main/java/happyTroublers/destination/DestinationController.java
@@ -1,4 +1,52 @@
 package happyTroublers.destination;
 
+import happyTroublers.destination.dtos.DestinationRequest;
+import happyTroublers.destination.dtos.DestinationResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/destinations")
+
 public class DestinationController {
+    private final DestinationService DESTINATION_SERVICE;
+
+    public DestinationController(DestinationService destinationService) {
+        DESTINATION_SERVICE = destinationService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DestinationResponse>> getAllDestinations(){
+        List<DestinationResponse> destinations = DESTINATION_SERVICE.getAllDestinations();
+        return new ResponseEntity<>(destinations, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DestinationResponse> getDestinationById(@PathVariable Long id) {
+        DestinationResponse destinationResponse = DESTINATION_SERVICE.getDestinationById(id);
+        return new ResponseEntity<>(destinationResponse, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<DestinationResponse> addDestination(@Valid @RequestBody DestinationRequest destinationRequest) {
+        DestinationResponse destinationResponse = DESTINATION_SERVICE.addDestination(destinationRequest);
+        return new ResponseEntity<>(destinationResponse, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DestinationResponse> updateDestination(@PathVariable Long id, @Valid @RequestBody DestinationRequest destinationRequest) {
+        DestinationResponse destinationResponse = DESTINATION_SERVICE.updateDestination(id, destinationRequest);
+            return ResponseEntity<>(destinationResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDestination(@PathVariable Long id){
+        DESTINATION_SERVICE.deleteDestination(id);
+        return ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
