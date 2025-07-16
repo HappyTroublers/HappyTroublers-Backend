0a41d73fed52a4e4cb6ea95bcd75d699cb468e75
Bruna Sonda <bsonda@gmail.com>
2025-07-16 12:17:45 +0200

    feat(Destination): create customized constructor

--- src/main/java/happyTroublers/destination/Destination.java
+++ src/main/java/happyTroublers/destination/Destination.java
@@ -33,4 +33,13 @@ public class Destination {
     @ManyToOne
     @JoinColumn(name = "customUser_id", nullable = false)
     private CustomUser user;
+
+    public Destination(String city, String country, String description, String imageUrl, CustomUser user) {
+        this.city = city;
+        this.country = country;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.user = user;
+
+    }
 }
