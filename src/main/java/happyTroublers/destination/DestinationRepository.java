40c841a5b2b9c812de1035bf1dbe41c292f248a9
Bruna Sonda <bsonda@gmail.com>
2025-07-23 15:05:43 +0200

    feat(DestinationRepository/Service): add findByUsername and getDestinationByUsername methods

--- src/main/java/happyTroublers/destination/DestinationRepository.java
+++ src/main/java/happyTroublers/destination/DestinationRepository.java
@@ -2,6 +2,9 @@ package happyTroublers.destination;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DestinationRepository extends JpaRepository<Destination, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface DestinationRepository extends JpaRepository<Destination, Long> {
+    Optional<List<Destination>> findByUsername(String Username);
 }
