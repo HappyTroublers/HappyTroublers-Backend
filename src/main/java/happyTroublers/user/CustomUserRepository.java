4f099faa91e46bb65d39db6eda3594ad4d8d0407
Bruna Sonda <bsonda@gmail.com>
2025-07-23 14:31:43 +0200

    fix(Destination, CustomUserRepository, DestinationController): modify endpoint in CustomUserRepository, add again findByUsername method and replace @Getters and @Setters by @Data

--- src/main/java/happyTroublers/user/CustomUserRepository.java
+++ src/main/java/happyTroublers/user/CustomUserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CustomUserRepository extends JpaRepository <CustomUser, Long> {
+    Optional<CustomUser> findByUsername(String username);
 }
