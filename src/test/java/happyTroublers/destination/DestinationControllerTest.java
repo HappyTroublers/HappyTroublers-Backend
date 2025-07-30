5dee437dc410a0e30929d2289d09b0dc23dbe578
Lara Pla <laraplamoreno@gmail.com>
2025-07-31 00:45:16 +0200

    test(DestinationControllerTest): add getDestinationById test

--- src/test/java/happyTroublers/destination/DestinationControllerTest.java
+++ src/test/java/happyTroublers/destination/DestinationControllerTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import happyTroublers.destination.dtos.DestinationResponse;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -27,36 +29,48 @@ public class DestinationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private DestinationService destinationService;
-
     @Autowired
     private ObjectMapper objectMapper;
 
-    private List<DestinationResponse> destinationResponseList;
+    @MockBean
+    private DestinationService destinationService;
+
+    private DestinationResponse destinationResponse;
+    private static final String BASE_URL = "/destinations";
 
     @BeforeEach
     void setUp() {
-
-        destinationResponseList = new ArrayList<>();
-
-        DestinationResponse destinationResponse1 = new DestinationResponse("Madrid", "Spain", "blablabla", "img.png", "María");
-        DestinationResponse destinationResponse2 = new DestinationResponse("London", "UK", "blibli", "img1.png", "María");
-
-        destinationResponseList.add(destinationResponse1);
-        destinationResponseList.add(destinationResponse2);
+        destinationResponse = new DestinationResponse("Madrid", "Spain", "blablabla", "img.png", "María");
     }
 
-    @Test
-    void shouldGetAllDestinationsSuccessfully() throws Exception {
+    @Nested
+    @DisplayName("GET endpoints")
+    class GetUserTests {
 
-        given(destinationService.getAllDestinations()).willReturn(destinationResponseList);
+        @Test
+        @DisplayName("GET /destinations - should return all destinations")
+        void getAllDestinations_returnsListOfDestinations() throws Exception {
+            List<DestinationResponse> list = List.of(destinationResponse);
+            given(destinationService.getAllDestinations()).willReturn(list);
 
-        mockMvc.perform(get("/destinations").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", Matchers.hasSize(2)))
-                .andExpect(jsonPath("$[0].city").value("Madrid"))
-                .andExpect(jsonPath("$[1].city").value("London"))
-                .andExpect(jsonPath("$[0].username").value("María"));
+            mockMvc.perform(get("/destinations").accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                    .andExpect(jsonPath("$[0].city").value("Madrid"))
+                    .andExpect(jsonPath("$[0].country").value("Spain"))
+                    .andExpect(jsonPath("$[0].username").value("María"));
+        }
+
+        @Test
+        @DisplayName("GET /destinations/{id} - should return destination by ID")
+        void getDestinationById_returnsCorrectDestination() throws Exception {
+            given(destinationService.getDestinationById(1L, "María")).willReturn(destinationResponse);
+
+            mockMvc.perform(get("/destinations/1").param("username", "María"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.city").value("Madrid"))
+                    .andExpect(jsonPath("$.country").value("Spain"))
+                    .andExpect(jsonPath("$.username").value("María"));
+        }
     }
 }
